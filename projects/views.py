d85526ac423ce301419fc57f920797efc9fa423a
Hugo Belém <belem.hugo@hotmail.com>
2024-09-13 23:22:14 -0300

    building the database

---
+++ projects/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from .models import Project
+
+
+def projects(request):
+    projects = Project.objects.all()
+    context = {'projects': projects}
+    return render(request, 'projects/projects.html', context)
+
+
+def project(request, pk):
+    project = Project.objects.get(id=pk)
+
+    context = {'project': project}
+    return render(request, 'projects/single-project.html', context)
+
