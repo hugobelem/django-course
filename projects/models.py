50d905fdbd6bf03c5d0838f1708dcfd05e36194e
Hugo Belém <belem.hugo@hotmail.com>
2024-09-14 16:40:57 -0300

    17. User App

--- projects/models.py
+++ projects/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 import uuid
 
+from users.models import Profile
+
 
 class Project(models.Model):
+    owner = models.ForeignKey(Profile, null=True,blank=True,
+                              on_delete=models.SET_NULL)
     title = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
