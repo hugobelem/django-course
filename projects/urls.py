8f02034f662717d564d9436c906e739db2fd7e52
Hugo Belém <belem.hugo@hotmail.com>
2024-09-14 16:40:57 -0300

    17. User App

--- projects/urls.py
+++ projects/urls.py
@@ -5,9 +5,9 @@ from . import views
 
 urlpatterns = [
     path('', views.projects, name='projects'),
-    path('project/<str:pk>/', views.project, name='project'),
-    path('project/create', views.create_project, name='create'),
-    path('project/update/<str:pk>/', views.update_project, name='update'),
-    path('project/delete/<str:pk>/', views.delete_project, name='delete'),
+    path('<str:pk>/', views.project, name='project'),
+    path('create', views.create_project, name='create'),
+    path('update/<str:pk>/', views.update_project, name='update'),
+    path('delete/<str:pk>/', views.delete_project, name='delete'),
 ]
 
