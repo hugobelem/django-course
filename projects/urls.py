6627317a952c643c3a61ff6575a6c0f222d63924
Hugo Belém <belem.hugo@hotmail.com>
2024-09-14 11:57:42 -0300

    14. CRUD

--- projects/urls.py
+++ projects/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.projects, name='projects'),
     path('project/<str:pk>/', views.project, name='project'),
     path('project/create', views.create_project, name='create'),
+    path('project/update/<str:pk>/', views.update_project, name='update'),
+    path('project/delete/<str:pk>/', views.delete_project, name='delete'),
 ]
