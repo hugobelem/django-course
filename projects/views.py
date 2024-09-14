d29c857de8455f96041ba0bfbcea08b4f246bc49
Hugo Belém <belem.hugo@hotmail.com>
2024-09-14 11:57:42 -0300

    14. CRUD

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Project
 from .forms import ProjectForm
@@ -19,4 +19,36 @@ def project(request, pk):
 
 def create_project(request):
     context = {'form': ProjectForm}
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
     return render(request, 'projects/project-form.html', context)
+
+
+def update_project(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+        
+    context = {'form': form}
+    return render(request, 'projects/project-form.html', context)
+
+
+def delete_project(request, pk):
+    project = Project.objects.get(id=pk)
+
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+
+    context = {'object': project}
+    return render(request, 'projects/delete-template.html', context)
